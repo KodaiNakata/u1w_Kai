c923b6491813f0fd2610c058ecb1e5440f1fc55c
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2021-02-23 21:25:35 +0900

    スタート画面(#1)からゲーム画面(#2)へ遷移

---
+++ Kai/Assets/Script/StartController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// スタート画面操作用のクラス
+/// </summary>
+public class StartController : MonoBehaviour
+{
+    /// <summary>
+    /// 音源
+    /// </summary>
+    [SerializeField]
+    private AudioSource audioSource;
+
+    /// <summary>
+    /// クリックされたか
+    /// </summary>
+    private bool isClicked;
+
+    /// <summary>
+    /// 最初のUpdate前に呼び出される関数
+    /// </summary>
+    void Start()
+    {
+        isClicked = false;
+    }
+
+    /// <summary>
+    /// 1フレームごとに呼ばれる関数
+    /// </summary>
+    void Update()
+    {
+        // 左クリックしたとき
+        if (Input.GetMouseButtonDown(0) && !isClicked)
+        {
+            isClicked = true;
+            audioSource.PlayOneShot(audioSource.clip);
+            StartCoroutine(LoadGameScene());
+        }
+    }
+
+    /// <summary>
+    /// ゲーム画面をロードする
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator LoadGameScene()
+    {
+        yield return new WaitForSeconds(audioSource.clip.length);
+        SceneManager.LoadScene("GameScene");
+    }
+}
