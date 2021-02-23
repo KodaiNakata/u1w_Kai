f242e96cd260f0f0031f21b832bd9c964a096939
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2021-02-23 21:26:03 +0900

    ゲーム画面(#2)関連のスクリプトを作成中

---
+++ Kai/Assets/Script/Bomb.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 爆弾のクラス
+/// </summary>
+public class Bomb : MonoBehaviour
+{
+    /// <summary>
+    /// 爆破タイム
+    /// </summary>
+    private const float BLAST_TIME = 6000f;
+
+    /// <summary>
+    /// 爆発するまでのタイム
+    /// </summary>
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0f;
+    }
+
+    /// <summary>
+    /// 1フレームごとに呼ばれる関数
+    /// </summary>
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > BLAST_TIME)
+        {
+            //TODO：爆破アニメーションへ
+
+        }
+    }
+}
---
+++ Kai/Assets/Script/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーのクラス
+/// </summary>
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// 爆弾を持っているか
+    /// </summary>
+    private bool hasBomb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// 1フレームごとに呼ばれる関数
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
