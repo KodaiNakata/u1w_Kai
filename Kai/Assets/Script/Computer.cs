c4156cd32fe0b1e6be9401a060d9a7b291a34b06
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2021-02-28 20:22:37 +0900

    手の操作関連のスクリプトを追加(#2)

--- Kai/Assets/Script/Computer.cs
+++ Kai/Assets/Script/Computer.cs
@@ -2,17 +2,133 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// コンピュータのクラス
+/// </summary>
 public class Computer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 手のオブジェクトのリスト
+    /// </summary>
+    [SerializeField]
+    private List<GameObject> handObjects;
+
+    /// <summary>
+    /// コンピュータのアニメーター
+    /// </summary>
+    private Animator comAnimator;
+
+    /// <summary>
+    /// コンピュータの音源
+    /// </summary>
+    private AudioSource comAudioSource;
+
+    /// <summary>
+    /// 爆弾を投げるまでのタイム
+    /// </summary>
+    private float time;
+
+    /// <summary>
+    /// 爆弾を投げるまでの間隔
+    /// </summary>
+    private float timeSpan;
+
+    /// <summary>
+    /// 敗者か
+    /// </summary>
+    private bool isLoser;
+
+    /// <summary>
+    /// 爆弾を持っているか
+    /// </summary>
+    public bool hasBomb { get; set; }
+
+    /// <summary>
+    /// 最初のUpdate前に呼び出される関数
+    /// </summary>
+    void OnEnable()
     {
-        
+        comAudioSource = GetComponent<AudioSource>();
+        hasBomb = false;
+        comAnimator = GetComponent<Animator>();
+        time = 0f;
+        timeSpan = 5f;
+        isLoser = false;
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// 1フレームごとに呼ばれる関数
+    /// </summary>
     void Update()
     {
-        
+        BlastHand();
+        ThrowBomb();
+    }
+
+    /// <summary>
+    /// 爆弾を投げる
+    /// </summary>
+    private void ThrowBomb()
+    {
+        time += Time.deltaTime;
+
+        // 爆弾を持っている状態の時
+        if (!Bomb.isThrown && hasBomb && Bomb.inHand && time >= timeSpan)
+        {
+            ThrowRandomHand();
+            timeSpan = Random.Range(5f, 8f);
+            time = 0f;
+        }
+    }
+
+    /// <summary>
+    /// ランダムに他の手へ投げる
+    /// </summary>
+    /// <returns></returns>
+    private void ThrowRandomHand()
+    {
+        if (!comAnimator.GetBool("lose"))
+        {
+            int randomHandListNo = Random.Range(0, handObjects.Count);
+            do
+            {
+                if (handObjects[randomHandListNo].CompareTag("Player"))
+                {
+                    break;
+                }
+                else if (!handObjects[randomHandListNo].GetComponent<Computer>().isLoser)
+                {
+                    break;
+                }
+                randomHandListNo = Random.Range(0, handObjects.Count);
+            } while (true);
+            Bomb.endPos = handObjects[randomHandListNo].transform.position;
+            Bomb.isThrown = true;
+            comAnimator.SetBool("doThrowing", true);
+        }
+    }
+
+    /// <summary>
+    /// 手が爆発したか判断する
+    /// </summary>
+    private void BlastHand()
+    {
+        // 爆弾を持っていて爆発したとき
+        if (hasBomb && Bomb.doBlast)
+        {
+            // 敗者のアニメーションへ遷移
+            comAnimator.SetBool("lose", true);
+            GameManager.instance.CreateBomb();
+            hasBomb = false;
+            isLoser = true;
+        }
+    }
+
+    /// <summary>
+    /// 投げるSEを再生する
+    /// </summary>
+    private void PlayThrowSE()
+    {
+        comAudioSource.PlayOneShot(comAudioSource.clip);
     }
 }
--- Kai/Assets/Script/Player.cs
+++ Kai/Assets/Script/Player.cs
@@ -8,14 +8,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     /// <summary>
+    /// プレイヤーのアニメータ
+    /// </summary>
+    private Animator playerAnimator;
+
+    /// <summary>
     /// 爆弾を持っているか
     /// </summary>
-    private bool hasBomb;
+    public static bool hasBomb { get; set; }
 
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// プレイヤーの音源
+    /// </summary>
+    private AudioSource playerAudioSource;
+
+    /// <summary>
+    /// 最初のUpdate前に呼び出される関数
+    /// </summary>
+    void OnEnable()
     {
-        
+        playerAnimator = GetComponent<Animator>();
+        playerAudioSource = GetComponent<AudioSource>();
+        hasBomb = true;
     }
 
     /// <summary>
@@ -23,6 +37,53 @@ public class Player : MonoBehaviour
     /// </summary>
     void Update()
     {
-        
+        ThrowBomb();
+        BlastHand();
+    }
+
+    /// <summary>
+    /// 爆弾を投げる
+    /// </summary>
+    private void ThrowBomb()
+    {
+        // 爆弾を持っている状態で左クリックした瞬間のとき
+        if (hasBomb && Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d.transform != null)
+            {
+                // クリックしたオブジェクトがコンピュータで敗者でないとき
+                if (hit2d.transform.gameObject.CompareTag("COM") && !hit2d.transform.GetComponent<Animator>().GetBool("lose"))
+                {
+                    // 爆弾を投げる
+                    playerAnimator.SetBool("doThrowing", true);
+                    Bomb.isThrown = true;
+                    Bomb.endPos = hit2d.transform.position;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 手が爆発したか判断する
+    /// </summary>
+    private void BlastHand()
+    {
+        // 爆弾を持っていて爆発したとき
+        if (hasBomb && Bomb.doBlast)
+        {
+            // 敗者のアニメーションへ遷移
+            playerAnimator.SetBool("lose", true);
+        }
+    }
+
+    /// <summary>
+    /// 投げるSEを再生する
+    /// </summary>
+    private void PlayThrowSE()
+    {
+        playerAudioSource.PlayOneShot(playerAudioSource.clip, SoundManager.instance.seVolume);
     }
 }
