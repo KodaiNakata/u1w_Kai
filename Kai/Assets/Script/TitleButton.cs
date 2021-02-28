6e66b53ac80cafe9ccb36d73e0e349a394c7de05
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2021-02-28 20:20:19 +0900

    ゲーム画面(#2)にタイトルボタンを追加

---
+++ Kai/Assets/Script/TitleButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// タイトルボタンのクラス
+/// </summary>
+public class TitleButton : MonoBehaviour
+{
+    /// <summary>
+    /// クリック時の処理
+    /// </summary>
+    public void OnClick()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
