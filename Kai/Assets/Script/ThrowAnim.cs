31ec2ffe2afbee0d96e131c1f31d4aa009316378
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2021-02-28 20:21:23 +0900

    投げるアニメーション終了後に待機アニメーションへ移行(#2)

---
+++ Kai/Assets/Script/ThrowAnim.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 投げるアニメーションのクラス
+/// </summary>
+public class ThrowAnim : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    /// <summary>
+    /// アニメーション終了時の処理
+    /// </summary>
+    /// <param name="animator">アニメータ</param>
+    /// <param name="stateInfo">アニメータの状態の情報</param>
+    /// <param name="layerIndex">レイヤーのインデックス</param>
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("doThrowing", false);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
